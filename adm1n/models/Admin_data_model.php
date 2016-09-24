ec9f32415d2c603bd09dde8d3fd9f021643390f2
lynalex <j1234jy@gmail.com>
2016-09-24 20:33:58 +0800

    获取管理员信息

---
+++ adm1n/models/Admin_data_model.php
@@ -0,0 +1,20 @@
+<?php
+
+	class Admin_data_model extends CI_model
+	{
+			function __construct()
+			{
+					parent::__construct();
+					$this->load->database();
+			}
+			
+			public function get()
+			{
+				$sql = "select * from ADMIN_QWE";
+				$re = $this->db->query($sql);
+				$res = $re->result_array();
+				return $res;
+			}
+	}
+
+?>
