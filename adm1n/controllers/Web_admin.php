5512cf42fb1629c81373f15269d91061548f9265
lynalex <j1234jy@gmail.com>
2016-09-24 20:33:11 +0800

    新增方法

--- adm1n/controllers/Web_admin.php
+++ adm1n/controllers/Web_admin.php
@@ -1,10 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Web_admin extends CI_Controller {
-
+class Web_admin extends CI_Controller 
+{
+	private $admin;
+	private $pass;
+	function __construct()
+	{
+		parent::__construct();
+		$this -> load -> library('session'); 
+		$this -> load -> library('form_validation'); 
+		$this -> load -> model('Admin_data_model');
+		$data = $this -> Admin_data_model -> get();
+		var_dump($data);
+	}
+	
 	public function index()
 	{
-		phpinfo();
+		//$is_login = $this -> session -> userdata('is_login');
+		//$admin = $this -> session -> userdata('admin');
+		echo 1;
 	}
 }
