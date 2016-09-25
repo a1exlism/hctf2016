be2997c0f95436941ce99fc6460814cc51965e3a
lynalex <j1234jy@gmail.com>
2016-09-25 12:27:42 +0800

    管理首页

---
+++ views/adm1n/admin_view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ADMIN</title>
+</head>
+<body>
+<button id="team">team manage</button>
+<button id="challenge">challenge manage</button>
+<button id="info">info manage</button>
+
+</body>
+</html>
