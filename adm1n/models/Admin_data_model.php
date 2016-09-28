1769e5814de6ed6c920d57dae477a6d995b1fd58
lynalex <j1234jy@gmail.com>
2016-09-28 10:34:08 +0800

    完善操作

--- adm1n/models/Admin_data_model.php
+++ adm1n/models/Admin_data_model.php
@@ -1,11 +1,12 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 	class Admin_data_model extends CI_model
 	{
 			function __construct()
 			{
-					parent::__construct();
-					$this->load->database();
+				parent::__construct();
+				$this->load->database();
 			}
 			
 			public function get()
