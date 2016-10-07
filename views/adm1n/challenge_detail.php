e5cdd4c88b302ecc734bdbe1b6759e426fa7a837
lynalex <j1234jy@gmail.com>
2016-10-07 15:27:26 +0800

    修正视图

---
+++ views/adm1n/challenge_detail.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ADMIN</title>
+	<script type="text/javascript">
+function _update()
+{
+document.form.action="/hctf2016/adm1n/challenge/change";
+document.form.submit();
+}
+function _delete()
+{
+document.form.action="/hctf2016/adm1n/challenge/delete";
+document.form.submit();
+}
+</script>
+</head>
+
+<body>
+
+<form name="form" method="post">
+	<input type="text" name="id" value=<?php echo '"'.$result[0]['challenge_id'].'"';?>>
+	<br>
+	<input type="text" name="score" value=<?php echo '"'.$result[0]['challenge_score'].'"';?>>
+	<br>
+	<input type="text" name="description" value=<?php echo '"'.$result[0]['challenge_description'].'"';?>>
+	<br>
+	<input type="text" name="level" value=<?php echo '"'.$result[0]['challenge_level'].'"';?>>
+	<br>
+	<input type="text" name="hit" value=<?php echo '"'.$result[0]['challenge_hit'].'"';?>>
+	<br>
+	<input type="text" name="api" value=<?php echo '"'.$result[0]['challenge_api'].'"';?>>
+	<br>
+	<input type="button" value="UPDATE" onclick=_update()>
+	<input type="button" value="DELETE" onclick=_delete()>
+</form>
+</html>
--- views/adm1n/show_challenge.php
+++ views/adm1n/show_challenge.php
@@ -4,34 +4,25 @@
 	<title>ADMIN</title>
 </head>
 <body>
-<?php foreach( $result as $value ):?>
-	<table>
+<table>
 	<tr>
-		<td>id:</td>
-		<td> <?=$value['challenge_id'];?> </td>
-	</tr>
-	<tr>
-		<td>score:</td>
-		<td><?=$value['challenge_score'];?></td>
-	</tr>
-	<tr>
+		<td>id</td>
+		<td>score</td>
 		<td>description</td>
-		<td><?=$value['challenge_description']?></td>
-	</tr>
-	<tr>
 		<td>level</td>
-		<td><?=$value['challenge_level'];?></td>
-	</tr>
-	<tr>
 		<td>hit</td>
-		<td><?=$value['challenge_hit'];?></td>
-	</tr>
-	<tr>
 		<td>api</td>
+	</tr>
+	<?php foreach( $result as $value ):?>
+	<tr>
+		<td><a href="/hctf2016/adm1n/challenge/detail/<?=$value['challenge_id'];?>"><?=$value['challenge_id'];?></a></td>
+		<td><?=$value['challenge_score'];?></td>
+		<td><?=$value['challenge_description'];?></td>
+		<td><?=$value['challenge_level'];?></td>
+		<td><?=$value['challenge_hit'];?></td>
 		<td><?=$value['challenge_api'];?></td>
 	</tr>
+		<?php endforeach;?>
 	</table>
-	<hr />
-	<?php endforeach;?>
 </body>
 </html>
