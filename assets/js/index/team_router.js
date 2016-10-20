771be94a17388d60fd21471504faf82105a4cc94
Quentin Smith <shengjitian@gmail.com>
2016-10-20 14:39:38 +0800

    Auto load css/js

--- assets/js/index/team_router.js
+++ assets/js/index/team_router.js
@@ -4,26 +4,65 @@
 'use strict'
 $(function () {
 	//  settings 作为主页
+	
+	function addCSS(url) {
+		$('<link>').attr({
+			rel: 'stylesheet',
+			type: 'text/css',
+			href: url
+		}).appendTo('head');
+	}
+	
+	//  动态加载静态资源
+	function getSource(sname, stype) {
+		stype = stype || 'js';  //默认以js加载
+		$.ajax({
+			url: 'Team_ajax/get_source',
+			method: 'POST',
+			dataType: 'json',
+			data: {
+				name: sname,
+				type: stype
+			},
+			//  成功就把数据加载进来
+			success: function (data) {
+				var resultUrl = data.url.replace(/[\\]/g,'');
+				console.log(data);
+				if (data.type == 'js') {
+					$.ajax({
+						url: resultUrl,
+						method: 'GET',
+						dataType: 'script'
+					});
+				} else {
+					addCSS(resultUrl);
+				}
+			}
+		})
+	}
+	
 	var getBulletin = function () {
 		$('#main-container').empty();
 		$('#main-container').load('Team/bulletin');
+		getSource('team_bulletin', 'css');
+		getSource('team_bulletin', 'js');
 	};
-
+	
 	var getChanllenge = function () {
 		$('#main-container').empty();
 		$('#main-container').load('Team/challenge');
 	};
-
+	
 	var getRank = function () {
 		$('#main-container').empty();
 		$('#main-container').load('Team/rank');
 	};
-
+	
 	var getSolved = function () {
 		$('#main-container').empty();
 		$('#main-container').load('Team/solved');
 	};
-
+	
 	var getSettings = function () {
 		$('#main-container').empty();
 		$('#main-container').load('Team/settings');
@@ -34,5 +73,4 @@ $(function () {
 	$('#toggle-rank').click(getRank);
 	$('#toggle-solved').click(getSolved);
 	$('#toggle-settings').click(getSettings);
-	
 });
--- index/controllers/Team_ajax.php
+++ index/controllers/Team_ajax.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: a1exlism
- * Date: 16-10-19
- * Time: 下午4:51
- */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Team_ajax extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->library('session');
+		//$this->load->model('team_model');
+		$this->load->model('session_check');
+
+		$this->load->helper('form');
+		$this->load->library('form_validation');  //表单验证类
+
+	}
+
+	public function index()
+	{
+
+	}
+
+	public function get_source()
+	{
+		$source = array(
+			'name' => $this->input->post('name', TRUE),
+			'type' => $this->input->post('type', TRUE)
+		);
+		$url = base_url('assets/'.$source['type'].'/index/'.$source['name'].'.'.$source['type']);
+		$arr = array(
+			'type' => $source['type'],
+			'url'  => $url
+		);
+		echo json_encode($arr);
+	}
+}
