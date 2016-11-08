fc8248bf41161ee126d3e6b7e111878a57cf144f
lynalex <j1234jy@gmail.com>
2016-11-09 00:34:17 +0800

    增加自动生成api功能

---
+++ adm1n/controllers/Createapi.php
@@ -0,0 +1,48 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class createapi extends CI_Controller 
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this -> load -> library('session'); 
+		$this -> load ->helper('file'); 
+		$this -> load -> model('Admin_data_model');
+		$this -> load -> model('session_check');
+
+		if(!$this->session_check->check())
+		{
+			redirect('/adm1n/Web_admin/index');
+		}
+
+		$data = $this -> Admin_data_model -> get();
+	}
+
+	public function api()
+	{
+		$name=$this->input->post('apiname');
+		$name=$this->security->xss_clean($name);
+
+		$flag=$this->input->post('flag');
+		$flag=$this->security->xss_clean($flag);
+		$data="<?php defined('BASEPATH') OR exit('No direct script access allowed'); class $name {public function getflag(".'$token'."){".'$flag='."'$flag';return ".'$flag;}} ?>';
+		$path="../index/libraries/".$name.'.php';
+
+		//echo $path;
+		if ( ! write_file($path, $data,'wb'))
+		{
+    		echo 'Unable to write the file';
+		}		
+
+		#function getflag($token){return $flag;}
+	}
+
+	public function index()
+	{
+		$this->load->view('adm1n/create_api');
+	}
+}
+
+?>
