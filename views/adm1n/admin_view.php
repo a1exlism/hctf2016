b1beade914135637788e3bce070ec3b946617f6b
lynalex <j1234jy@gmail.com>
2016-11-09 14:54:21 +0800

    后台增加退出功能

--- views/adm1n/admin_view.php
+++ views/adm1n/admin_view.php
@@ -8,6 +8,7 @@
 <a href="/hctf2016/adm1n/challenge/index">challenge manage</a>
 <a href="/hctf2016/adm1n/info/index">info manage</a>
 <a href="/hctf2016/adm1n/createapi/index">create api</a>
+<a href="/hctf2016/adm1n/web_admin/logout">logout</a>
 
 </body>
 </html>
