132285f2745521384de4dc9ddf17ad9f60975444
Quentin Smith <shengjitian@gmail.com>
2016-11-15 17:19:12 +0800

    check_flag merge

--- index/controllers/Check_flag.php
+++ index/controllers/Check_flag.php
@@ -32,16 +32,9 @@ class Check_flag extends CI_Controller
 		//$this->token='token';
 		$bool = $this->flag_model->check($id, $flag, $this->token);
 		#bool 0校验错误 1作弊 2校验正确 3flag已经正确提交 4没有开题
-<<<<<<< HEAD
 		if($bool==1)
 		{
-			$unset_data = array('team_token', 'is_login');
-			$this->session->unset_userdata($unset_data);
-			$this->db->update('team_info',array('team_pass'=>'cheat'),array('team_token'=>$this->token));
-=======
-		if ($bool == 1) {
 			$this->reset();
->>>>>>> d1504ea99c02ff0b72db7ebe40bb3d341c9a8648
 		}
 		echo json_encode(array("statusCode" => $bool));
 	}
